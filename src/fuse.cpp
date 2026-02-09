f844912367c702dfb8dca18f6443503c798ff8a3
Sebastian Brunnert <se.brunnert@gmail.com>
2026-02-09 22:38:33 +0100

    License

--- src/fuse.cpp
+++ src/fuse.cpp
@@ -1,6 +1,26 @@
-#include <iostream>
+/*
+ * Copyright (c) 2026 Sebastian Brunnert <mail@sebastianbrunnert.de>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+// We use FUSE 3.18, which is the latest stable release as of February 2026.
+#define FUSE_USE_VERSION FUSE_MAKE_VERSION(3, 18)
+
+#include <fuse_lowlevel.h>
 
 int main(int argc, char *argv[]) {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+
 }
