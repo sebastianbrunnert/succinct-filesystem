5a2c22f7b2af7b1acde0146a6d207f4c672fec81
Sebastian Brunnert <se.brunnert@gmail.com>
2026-03-03 22:16:06 +0100

    Test

--- src/fsm/delta/delta_stabilization.hpp
+++ src/fsm/delta/delta_stabilization.hpp
@@ -10,14 +10,50 @@
 class DeltaStabilization {
 private:
 
+    class DeltaOperation {
+        bool is_insert;
+        size_t inode;
+    };
+
+    std::vector<DeltaOperation> operations;
+
+
 public:
 
+    void record_insert(size_t inode) {
+        operations.push_back({true, inode});
+    }
+
+    void record_remove(size_t inode) {
+        operations.push_back({false, inode});
+    }
+
     size_t stable_inode_to_flouds_inode(size_t stable_inode) {
-        return stable_inode - 1;
+        size_t new_flouds_inode = stable_inode - 1;
+
+        for (const auto& op : operations) {
+            if (op.is_insert && op.inode <= new_flouds_inode) {
+                new_flouds_inode--;
+            } else if (!op.is_insert && op.inode < new_flouds_inode) {
+                new_flouds_inode++;
+            }
+        }
+
+        return new_flouds_inode;
     }
 
     size_t flouds_inode_to_stable_inode(size_t flouds_inode) {
-        return flouds_inode + 1;
+        size_t stable_inode = flouds_inode + 1;
+
+        for (const auto& op : operations) {
+            if (op.is_insert && op.inode <= flouds_inode) {
+                stable_inode++;
+            } else if (!op.is_insert && op.inode < flouds_inode) {
+                stable_inode--;
+            }
+        }
+
+        return stable_inode;
     }
 
 };
