93075d5aab0670a5f8ce27b58aa3f03273dbffe8
Sebastian Brunnert <se.brunnert@gmail.com>
2026-03-11 21:59:34 +0100

    Test

--- src/fsm/delta/delta_stabilization.hpp
+++ src/fsm/delta/delta_stabilization.hpp
@@ -12,28 +12,43 @@ private:
 
     struct DeltaOperation {
         bool is_insert;
-        size_t inode;
+        uint64_t inode;
     };
 
     std::vector<DeltaOperation> operations;
 
-
 public:
 
-    void record_insert(size_t inode) {
-        operations.push_back({true, inode});
+    void record_insert(uint64_t inode) {
+        operations.push_back({true, stable_inode_to_flouds_inode(inode)});
     }
 
-    void record_remove(size_t inode) {
-        operations.push_back({false, inode});
+    void record_remove(uint64_t inode) {
+        operations.push_back({false, stable_inode_to_flouds_inode(inode)});
     }
 
-    size_t stable_inode_to_flouds_inode(size_t stable_inode) {
-        return stable_inode - 1;
+    uint64_t stable_inode_to_flouds_inode(uint64_t stable_inode) {
+        uint64_t log_position = stable_inode >> 48;
+        uint64_t inode_number = stable_inode & (1ULL << 48) - 1;
+
+        return inode_number - 1;
+
+        /*
+        WITHOUT CACHING IT WOULD BE:
+        return stable_inode - 1;        
+        */
     }
 
-    size_t flouds_inode_to_stable_inode(size_t flouds_inode) {
-        return flouds_inode + 1;
+    uint64_t flouds_inode_to_stable_inode(uint64_t flouds_inode) {
+        uint64_t log_position = operations.size();
+        uint64_t inode_number = flouds_inode + 1;
+
+        return (log_position << 48) | inode_number;
+
+        /*
+        WITHOUT CACHING IT WOULD BE:
+        return flouds_inode + 1;        
+        */
     }
 
 };
